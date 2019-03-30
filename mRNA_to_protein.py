34b2860bd4e26e912debfa887c7affca04ad86b6
Adele Valeria <valeria.18@intl.zju.edu.cn>
2019-03-30 17:16:50 +0800

    task 4 mRNA to protein

---
+++ mRNA_to_protein.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar 30 17:15:53 2019
+
+@author: Group2
+
+mRNA to protein
+Write a function that computes a polypeptide sequence from any user-specified mRNA sequence.
+For simplicity, you are allowed to assume that the mRNA contains exactly one start codon and one
+stop codon. 
+"""
+
