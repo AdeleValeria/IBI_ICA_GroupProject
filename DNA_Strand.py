fb01dc4b6c4478f9c9c98eec0558eb4b65ffa01c
Adele Valeria <valeria.18@intl.zju.edu.cn>
2019-03-30 17:14:30 +0800

    task 2 DNA strand calculator

---
+++ DNA_Strand.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar 30 17:12:59 2019
+
+@author: Group2
+
+Complementary DNA strand calculator
+Write a function that computes the complementary DNA strand (from 5’ to 3’) for any userspecified
+DNA sequence (assuming the user has also specified it 5’ to 3’)
+"""
+
