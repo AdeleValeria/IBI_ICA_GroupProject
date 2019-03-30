0bf4f3d351947766910ff47300728136943a9ba6
Adele Valeria <valeria.18@intl.zju.edu.cn>
2019-03-30 17:18:26 +0800

    task 5 Additional Function

---
+++ Additional_Function.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar 30 17:17:10 2019
+
+@author: Group2
+
+Additional function 
+Write an additional function that takes a nucleotide or polypeptide sequence as input and computes
+something interesting and/or biologically meaningful. You can be as creative as you like, but we
+prefer a simple idea that works to a complex idea that is poorly implemented.
+"""
+
