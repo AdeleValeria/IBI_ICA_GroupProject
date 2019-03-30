78842b6cee4aa64a1483620889bc8dae8a8ca735
Adele Valeria <valeria.18@intl.zju.edu.cn>
2019-03-30 17:15:36 +0800

    task 3 DNA to mRNA converter

---
+++ DNA_to_mRNA.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar 30 17:14:46 2019
+
+@author: Group2
+
+DNA to mRNA convertor
+Write a function that computes the mRNA sequence form any user-specified DNA sequence.
+"""
+
