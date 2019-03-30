1af83b7f20a3d27d4186e1a6266aec828a8608a6
Adele Valeria <valeria.18@intl.zju.edu.cn>
2019-03-30 17:12:37 +0800

    Added GC calculator instruction

--- GC_Content_Calculator.py
+++ GC_Content_Calculator.py
@@ -3,5 +3,16 @@
 Created on Sat Mar 30 16:58:24 2019
 
 @author:Group2
+
+GC Content Calculator
+GC content, i.e. the fraction of total DNA that is comprised of Guanine and Cytosine nucelotides,
+varies from species to species. Guanine-Cytosine base pairs use three hydrogen bonds, whereas
+Adenine-Thymine base pairs use only two. This means that the stability of the DNA double helix
+and consequently its “melting temperature”, i.e. the temperature that is required to separate both
+strands in the lab, varies depending on GC content. It is therefore useful to be able to compute the
+GC content.
+
+Write a function that computes the GC content (in % of total bases) for any user-specified DNA
+sequence. 
 """
-#GC Content Calculator
+
